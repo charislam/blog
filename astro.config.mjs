8fda642209a9f24da90f47b4d0e9422f01696998
Charis Lam <26616127+charislam@users.noreply.github.com>
2023-10-01 15:00:04 -0400

    feat: add server-side analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://charislam.com',
 	integrations: [mdx(), sitemap()],
-	output: "static",
-	adapter: vercel()
+	output: "server",
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		}
+	})
 });
